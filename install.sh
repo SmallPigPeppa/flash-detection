fcf0c9a0f0294b3f0b3f3defdf7fcc0c38a73d83
lwz <905328134@qq.com>
2024-04-05 23:05:38 +0800

    initial

---
+++ install.sh
@@ -0,0 +1,8 @@
+pip install lightning-flash
+pip install 'lightning-flash[image]'
+pip install 'lightning-flash[tabular]'
+pip install 'lightning-flash[text]'
+pip install torch==1.10.2+cu113 torchvision==0.11.3+cu113 torchaudio==0.10.2+cu113 torchtext==0.11.2 -f https://download.pytorch.org/whl/cu113/torch_stable.html
+pip install icevision==0.11.0
+
+pip install icedata
