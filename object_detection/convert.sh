49be8e2d24bb339e51e49e2731e87974d267ec08
lwz <905328134@qq.com>
2024-04-06 01:31:20 +0800

    initial

--- object_detection/convert.sh
+++ object_detection/convert.sh
@@ -1,2 +1,2 @@
-PATH_TO_CKPT=
-python3 convert_model_to_detectron2.py --pretrained_feature_extractor PATH_TO_CKPT --output_detectron_model ./detectron_model.pkl
+PATH_TO_CKPT=byol.ckpt
+python3 convert_model_to_detectron2.py --pretrained_feature_extractor ${PATH_TO_CKPT} --output_detectron_model ./detectron_model.pkl
